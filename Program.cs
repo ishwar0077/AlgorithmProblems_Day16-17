6f40c595ecb6cfd89e7fc2378b9add133d63271b
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-22 22:30:28 +0530

    Program for check anagram

--- Program.cs
+++ Program.cs
@@ -4,74 +4,28 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = { 76, 89, 23, 11, 5, 70, 87, 12, 44, 10 };
-            int n = 10, i;
-            Console.WriteLine("Merge Sort");
-            Console.Write("Initial array is: ");
-            for (i = 0; i < n; i++)
             {
-                Console.Write(arr[i] + " ");
-            }
-            mergeSort(arr, 0, n - 1);
-            Console.Write("\nSorted Array is: ");
-            for (i = 0; i < n; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-        }
-        static public void merge(int[] arr, int p, int q, int r)
-        {
-            int i, j, k;
-            int n1 = q - p + 1;
-            int n2 = r - q;
-            int[] L = new int[n1];
-            int[] R = new int[n2];
-            for (i = 0; i < n1; i++)
-            {
-                L[i] = arr[p + i];
-            }
-            for (j = 0; j < n2; j++)
-            {
-                R[j] = arr[q + 1 + j];
-            }
-            i = 0;
-            j = 0;
-            k = p;
-            while (i < n1 && j < n2)
-            {
-                if (L[i] <= R[j])
+                string str1 = "heart";
+                string str2 = "earth";
+
+                char[] ch1 = str1.ToLower().ToCharArray();
+                char[] ch2 = str2.ToLower().ToCharArray();
+
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+
+                string s1 = new string(ch1);
+                string s2 = new string(ch2);
+
+                if (s1 == s2)
                 {
-                    arr[k] = L[i];
-                    i++;
+                    Console.WriteLine("Anagram");
+
                 }
                 else
                 {
-                    arr[k] = R[j];
-                    j++;
+                    Console.WriteLine("Not Anagram");
                 }
-                k++;
-            }
-            while (i < n1)
-            {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-            while (j < n2)
-            {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
-        }
-        static public void mergeSort(int[] arr, int p, int r)
-        {
-            if (p < r)
-            {
-                int q = (p + r) / 2;
-                mergeSort(arr, p, q);
-                mergeSort(arr, q + 1, r);
-                merge(arr, p, q, r);
             }
         }
 
