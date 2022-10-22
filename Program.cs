d0e45424e41a43a4974d891a4cf1e97fcd83fd0a
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-22 23:57:01 +0530

    Program for Prime Palindrome

--- Program.cs
+++ Program.cs
@@ -1,34 +1,50 @@
-﻿namespace AlgorithmPrograms
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace AlgorithmPrograms
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-
-            int a, b, i, j, flag;
+            int a, b, rev, rem, flag;
             Console.WriteLine("Enter lower bound of " + "the interval: ");
             a = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter upper bound " + "of the interval: ");
             b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Prime numbers between " + "{0} and {1} are: ", a, b);
-            for (i = a; i <= b; i++)
-            {
 
-                if (i == 1 || i == 0)
-                    continue;
-                flag = 1;
-
-                for (j = 2; j <= i / 2; ++j)
+                int num, temp;
+                Console.WriteLine("Prime numbers between " + "{0} and {1} are: ", a, b);
+                for (num = a; num <= b; num++)
                 {
-                    if (i % j == 0)
+                    flag = 0;
+                    rev = 0;
+                    temp = num;
+                    for (int i = 1; i <= temp; i++)
                     {
-                        flag = 0;
-                        break;
+                        if (temp % i == 0)
+                        {
+                            flag++;
+                        }
+                    }
+                    if (flag == 2)
+                    {
+                        while (temp > 0)
+                        {
+                            rem = temp % 10;
+                            rev = rev * 10 + rem;
+                            temp = temp / 10;
+
+                        }
+                        if (rev == num)
+                        {
+                            Console.WriteLine(num + "");
+                        }
                     }
                 }
-                if (flag == 1)
-                    Console.WriteLine(i);
-            }
+
+            
         }
-    }
+        
+    } 
 }
